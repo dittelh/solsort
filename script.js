330ee145e2c1e69562883575f057a10fc578d304
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-09-28 11:19:00 +0200

    try add rain

--- script.js
+++ script.js
@@ -196,8 +196,52 @@ var anim = bodymovin.loadAnimation(animData);
 
 
 
+// Section 7
+// Rain
+
+  //clear out everything
+  document.querySelector('.rain').empty();
+
+  var increment = 0;
+  var drops = "";
+  var backDrops = "";
+
+  while (increment < 100) {
+    //couple random numbers to use for various randomizations
+    //random number between 98 and 1
+    var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+    //random number between 5 and 2
+    var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+    //increment
+    increment += randoFiver;
+    //add in a new raindrop with various randomizations to certain CSS properties
+    drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+  }
+
+  document.querySelector('.rain.front-row').insertAdjacentHTML("beforeend",drops);
+  document.querySelector('.rain.back-row').insertAdjacentHTML("beforeend",backDrops);
 
 
+document.querySelector('.splat-toggle.toggle').addEventListener('click', function() {
+  document.querySelector('body').classList.toggle('splat-toggle');
+  document.querySelector('.splat-toggle.toggle').classList.toggle('active');
+  makeItRain();
+});
+
+document.querySelector('.back-row-toggle.toggle').addEventListener('click', function() {
+  document.querySelector('body').classList.toggle('back-row-toggle');
+  document.querySelector('.back-row-toggle.toggle').classList.toggle('active');
+  makeItRain();
+});
+
+document.querySelector('.single-toggle.toggle').addEventListener('click', function() {
+  document.querySelector('body').classList.toggle('single-toggle');
+  document.querySelector('.single-toggle.toggle').classList.toggle('active');
+  makeItRain();
+});
+
+makeItRain();
 
 
 
